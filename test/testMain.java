e52fd10d4d1a970f5ce6d3974df4f12d37d31685
StevenCM199 <stevenCM199@gmail.com>
2020-05-20 18:17:45 -0600

    Initial commit

---
+++ test/testMain.java
@@ -0,0 +1,32 @@
+
+import junit.framework.TestCase;
+
+public class testMain extends TestCase{
+
+    private Main m;
+
+    public void escenario (){
+        m = new Main();
+    }
+
+    public void testsumar (){
+        escenario();
+        assertTrue(m.sumar(2 , 2) == (2 + 2));
+    }
+
+    public void testrestar (){
+        escenario();
+        assertTrue(m.restar(5 , 2 ) == (5-2));
+    }
+
+    public void testmulti (){
+        escenario();
+        assertTrue( m.multiplicar(2 , 3 ) == (2*3));
+    }
+
+    public void testdividir (){
+        escenario();
+        assertTrue( m.dividir(6 , 2 ) == (6/2));
+    }
+
+}
